2447a1829220434f50693030a00d32b9d7253a1d
YERUVA RAJASEKHAR REDDY <rsreddy27@yeruvas-mbp.attlocal.net>
2019-12-17 21:11:14 -0500

    my first commit

---
+++ src/test/java/com/myproject/soap/webservices/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.soap.webservices.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
